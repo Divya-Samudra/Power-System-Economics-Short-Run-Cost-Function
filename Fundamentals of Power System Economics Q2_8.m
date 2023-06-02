fe5a1ff3b754e0663be365d43ff5261f5abc692b
Divya Mohandas <130666521+Divya-Samudra@users.noreply.github.com>
2023-06-02 22:48:57 +0530

    Add files via upload

---
+++ Fundamentals of Power System Economics Q2_8.m
@@ -0,0 +1,21 @@
+clc; clear
+q=250; % Quantity manufactured
+for i1 = 1:q
+    Cost(i1) = 10*i1^2 + 200*i1 + 200000;
+    Required_cost(i1) = 2400*i1;
+    end
+
+%plots of Cost, Marginal Cost, Revenue and Profit 
+
+figure(1)
+x1 = 1:q;
+Cost1 = Cost(x1);
+set(gcf,'Name','Cost','Color','w')
+plot(x1,Cost1,'-k','LineWidth',2)
+hold on;
+plot(x1,Required_cost,'-r','LineWidth',2)
+hold on;
+xlabel('No. of Products')
+ylabel('Cost')
+title('Cost')
+legend('Cost','Required cost');
